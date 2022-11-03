e35d71841693b76947d82a7852aeadabd0748bb4
Hyun Ahn <codepawn@icloud.com>
2022-11-03 19:28:52 +0900

    reset_git cache

--- lib/toeplitz/solu.exs
+++ lib/toeplitz/solu.exs
@@ -1,9 +1,29 @@
 defmodule Solution do
   def perform(data) do
+    loop(true, data, %{})
+  end
 
+  def loop(_result, [head | tail], acc) do
+    head
+    |> Enum.map(fn
+      x ->
+        acc
+
+    end)
+
+    loop(tail)
+  end
+
+  def loop([]) do
+  end
+
+  def loop_y() do
+  end
+
+  def loop_y() do
   end
 end
 
 matrix = [[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]]
 
-Solution.perform
+matrix |> Solution.perform()
